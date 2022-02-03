cd781275c5cfa10b824f081ae7a272f2d4e25661
Elliot Fisk <elliotfisk@gmail.com>
2022-02-03 09:45:18 -0800

    fixed names to not pick up place names

--- PythonHelpers/names.py
+++ PythonHelpers/names.py
@@ -6,9 +6,13 @@ tablets = os.listdir(".\Translated")
 for tablet in tablets:
     output = open("./Names/" + tablet + "names.txt", "w", encoding="utf-8")
     tab = open("./Translated/" + tablet, "r", encoding="utf-8")
+    place = False
     currentLine = tab.readline()
     while currentLine != '':
-        test = currentLine.find("\tPN\n")
-        if(test != -1):
+        if(currentLine.find("\tPN\n") != -1 and place == False):
             output.write(currentLine)
+        if(currentLine.find("[place]") != -1):
+            place = True
+        else:
+            place = False
         currentLine = tab.readline()
