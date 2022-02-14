772c1214c9d5bf064b2907cd7a2767513f372175
Elliot Fisk <elliotfisk@gmail.com>
2022-02-14 09:36:23 -0800

    fixed nameGrabber adding place names

--- PythonHelpers/dataGrabber.py
+++ PythonHelpers/dataGrabber.py
@@ -1,16 +1,27 @@
-from fileinput import filename
+# Elliot D Fisk
+# Feb 2022
+# Program to handle all operations that grab data from raw translated
+# tablets and put it into a shared excel spreadsheet
+
+# Usage: add a boolean for each new grabber script and import that script.
+#        then pass the desired excel sheet from the tabulatedData workbook
+#        object into the grabber. (currently using wsNames, the first sheet)
+#        of tabulatedData.xlsx and path, which stores the path where the
+#        translated tablet files can be found.
+
 import openpyxl
 import os
 from nameGrabber import NameGrabber
 
 tabulatedData = openpyxl.load_workbook(filename = 'tabulatedData.xlsx')
+wsNames = tabulatedData.worksheets[0]
+path = os.getcwd() + '/Translated/'
 
 getNames = True
 getDates = False
 
-wsNames = tabulatedData.worksheets[0]
-
 if getNames:
-    n = NameGrabber(wsNames, os.getcwd() + '/Translated/')
+    n = NameGrabber(wsNames, path)
     n.namesToSheet()
+
     tabulatedData.save('tabulatedData.xlsx')
--- PythonHelpers/nameGrabber.py
+++ PythonHelpers/nameGrabber.py
@@ -1,3 +1,11 @@
+# Elliot Fisk
+# Feb 2022
+# Program to get names from tablets in self.path and print them
+# into self.sheet
+
+# Usage: Import to grabber and create new object with sheet and path
+#        then run namesToSheet.
+
 import os
 
 class NameGrabber(object):
@@ -18,14 +26,21 @@ class NameGrabber(object):
             self.sheet['A' + str(row)].value = tablet
             #start read
             currentline = tab.readline()
+            placeName = False
             #continue until end of tablet
             while currentline != '':
                 #split line around tab and tokenize
                 tokenline = currentline.split('\t')
+                if(currentline.find("[place]") != -1):
+                    placeName = True
                 if(currentline.find("\tPN\n") != -1):
-                    #if PN then add to spreadsheet
-                    self.sheet.cell(row, 3 + nameCount).value = tokenline[1]
-                    nameCount += 1
+                    if(placeName):
+                        #catch placeNames and dont add them to the spreadsheet
+                        placeName = False
+                    else:
+                        #if PN then add to spreadsheet
+                        self.sheet.cell(row, 3 + nameCount).value = tokenline[1]
+                        nameCount += 1
                 currentline = tab.readline()
             self.sheet.cell(row, 2).value = nameCount
             #increment to next line of spreadsheet
