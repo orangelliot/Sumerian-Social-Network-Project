7bc162a13ed5a92a35df07613187351c444e5bae
elliot <elliotfisk@gmail.com>
2022-03-31 16:18:11 -0700

    get me out

--- FamilyTree/tree.cpp
+++ FamilyTree/tree.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdlib.h>
 #include <string>
 #include <vector>
@@ -22,3 +23,29 @@ class Person{
 int main(int argc, char *argv[]){
 
 }
+=======
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Person{
+  protected:
+    string name;
+    vector <String> children
+  public:
+    Person(string name){
+      this-> name = name
+    }
+
+    string getName(){
+      return name;
+    }
+};
+
+int main(int argc, char *argv[]){
+
+}
+>>>>>>> 0d6286bb0abd9ac8555b89306a4908939bbcddd9
--- Python/Core/DataCollection/lengthGrabber.py
+++ Python/Core/DataCollection/lengthGrabber.py
@@ -1,6 +1,10 @@
 import os
 import openpyxl
 
+<<<<<<< HEAD
+=======
+chunks = os.listdir("PythonHelpers/Chunks")
+>>>>>>> 0d6286bb0abd9ac8555b89306a4908939bbcddd9
 tabulatedData = openpyxl.load_workbook(filename = 'tabulatedData.xlsx')
 hist = tabulatedData.worksheets[3]
 
@@ -8,6 +12,7 @@ row = 1
 currentCount = 0
 longest = 0
 
+<<<<<<< HEAD
 cur = open("PythonHelpers/Untranslated/ur3_untranslated.atf", 'r', encoding='utf-8')
 currentline = cur.readline()
 while currentline != '':
@@ -22,6 +27,23 @@ while currentline != '':
     if currentline[0].isnumeric():
         currentCount += 1
     currentline = cur.readline()
+=======
+for chunk in chunks:
+        cur = open("PythonHelpers/Chunks/" + chunk, 'r', encoding='utf-8')
+        currentline = cur.readline()
+        while currentline != '':
+            if currentline.find("&P") != -1 and currentline[8] == ' ':
+                if currentCount != 0:
+                    hist['B' + str(row)].value = currentCount
+                    if currentCount > longest:
+                        longest = currentCount
+                row += 1
+                hist['A' + str(row)].value = currentline[:8]
+                currentCount = 0
+            if currentline[0].isnumeric():
+                currentCount += 1
+            currentline = cur.readline()
+>>>>>>> 0d6286bb0abd9ac8555b89306a4908939bbcddd9
 hist['C1'].value = row
 hist['C2'].value = longest
 tabulatedData.save("tabulatedData.xlsx")
