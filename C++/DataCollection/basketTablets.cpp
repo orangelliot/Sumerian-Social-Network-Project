fe631795ccf770ce166121b1e0cd0195ce336d0d
ilh <ilh@wwu.edu>
2022-04-11 14:09:23 -0700

    Added basketTablets.cpp

---
+++ C++/DataCollection/basketTablets.cpp
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+int main(int argc, char *argv[]){
+  ofstream csv;
+  csv.open("Tree.csv");
+
+  csv << "TabletID" << endl;
+
+  string path = "../Dataset/Translated";
+  for(auto & entry : fs::directory_iterator(path)){
+
+  }
+
+  csv.close();
+}
