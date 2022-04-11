906123e137ef2e91eb801854b2406a947c784a7e
ilh <ilh@wwu.edu>
2022-04-11 14:32:16 -0700

    Added file reading and writing

--- C++/DataCollection/basketTablets.cpp
+++ C++/DataCollection/basketTablets.cpp
@@ -1,3 +1,5 @@
+//g++ -std=c++17 -o basketTablets.out basketTablets.cpp
+
 #include <stdlib.h>
 #include <string>
 #include <vector>
@@ -9,14 +11,22 @@ using namespace std;
 namespace fs = std::filesystem;
 
 int main(int argc, char *argv[]){
+  ifstream tablet;
   ofstream csv;
   csv.open("Tree.csv");
-
   csv << "TabletID" << endl;
 
   string path = "../../Dataset/Translated";
   for(auto & entry : fs::directory_iterator(path)){
+    tablet.open(entry.path());
 
+    string line = "";
+    while(getline(tablet, line) != EOF){
+      //split string
+      //check split for pisan dub-ba
+    }
+
+    tablet.close();
   }
 
   csv.close();
