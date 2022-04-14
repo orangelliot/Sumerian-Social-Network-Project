00a31bb108c2726d44fa3b045ffdba245646d80c
ilh <ilh@wwu.edu>
2022-04-13 18:19:06 -0700

    Splits the single atf tablet file into tablets

---
+++ C++/DataCollection/splitUntranslated.cpp
@@ -0,0 +1,33 @@
+//g++ -std=c++17 -o splitUntranslated.out splitUntranslated.cpp
+
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+int main(int argc, char *argv[]){
+  ifstream untranslated;
+  ofstream tablet;
+  untranslated.open("../../Dataset//ur3_untranslated.atf");
+
+  string path = "../../Dataset/Untranslated/";
+
+  string line;
+  string file;
+  while(getline(untranslated, line)){
+    if(line.length() < 9 && line.substr(0,2) == "&P"){
+      tablet.close();
+      file = path + line.substr(1, 8) + ".atf";
+      tablet.open(file);
+    }
+
+    tablet << line;
+  }
+
+  tablet.close();
+  untranslated.close();
+}
