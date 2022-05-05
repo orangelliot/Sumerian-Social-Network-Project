ca17a3edc72be15d64e97a9ccb1c45e969809a3b
ilh <ilh@wwu.edu>
2022-05-04 20:03:32 -0700

    Added SQL query templete for C++

---
+++ C++/DataCollection/sql/SQLQuery.cpp
@@ -0,0 +1,52 @@
+//g++ -std=c++17 -o SQLQuery.out SQLQuery.cpp -lmysqlcppconn
+
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/prepared_statement.h>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+const string server = "sumerian-social-network.clzdkdgg3zul.us-west-2.rds.amazonaws.com";
+const string username = "root";
+string password;
+
+int main(){
+  sql::Driver *driver;
+  sql::Connection *connect;
+  sql::PreparedStatement *prepState;
+  sql::ResultSet *result;
+
+  cout << "Password: ";
+  cin >> password;
+
+  try{
+    driver = get_driver_instance();
+    connect = driver->connect(server, username, password);
+  }
+  catch(sql::SQLException e){
+    cout << "Could not connect to server. Error message: " << e.what() << endl;
+    exit(1);
+  }
+
+  connect->setSchema("sumerianDB");
+
+  prepState = connect->prepareStatement("SELECT * FROM;");
+  result = prepState->executeQuery();
+
+  while(result->next()){
+
+  }
+
+  delete result;
+  delete prepState;
+  delete connect;
+  return 0;
+}
