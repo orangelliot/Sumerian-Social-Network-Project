c3288e6bf5675fbbecb3211c1b7817283db6a668
uhlhorn <uhlhorn@wwu.edu>
2022-05-10 09:17:57 -0700

    created skeleton of the build script for local database

---
+++ build.sh
@@ -0,0 +1,13 @@
+# Nicholas J Uhlhorn
+# May 2022
+# A shell script to automate making the database on a local machine
+
+# TODO: Check for dependencies and fetch them if they are not there
+    # The dataset, or a dataset
+        # Untranslated? if so we might want to run it through the pipline
+    # MySQL installed
+    # 
+
+# TODO: Setup database
+
+# TODO: Add entries to the database
