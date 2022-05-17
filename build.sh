c8caae50ec7def96daec10f61e0e8ab7fd101fbc
uhlhorn <uhlhorn@wwu.edu>
2022-05-17 15:28:45 -0700

    changed structure of build to ask all build questions before building anything

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 # May 2022
 # A shell script to automate making the database on a local machine
 
+########## CONSTANT VARIABLES ##########
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
@@ -10,13 +11,19 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 UNTRANSLATED=$"$SCRIPTPATH/Dataset/TestUntranslated"
 # Path to translated data
 TRANSLATED=$"$SCRIPTPATH/Dataset/TestTranslated" #!!!! CHANGE THIS FROM TEST
-
 # URL to Ur III period cdli data
 CDLIDATA=$"https://cdli.ucla.edu/tools/cdlifiles/cdli_ur3atf.zip"
 
-########## Check for direcories ##########
+########## User Prompt Variables #########
+grabUntranslatedData=$1
+translatedUntranslatedData=$1
+removePipeline=$1
+buildDatabase=$1
+populateDatabase=$1
 
-# Check if the directorys exists and create them if not
+########## Check for directories ##########
+
+# Check if the directories exists and create them if not
 if [ -d "$UNTRANSLATED" ]
 then
     echo "$UNTRANSLATED exists"
@@ -33,72 +40,100 @@ else
     mkdir -p -- "$TRANSLATED"
 fi
 
+##################################
+########## User Prompts ##########
+##################################
+# Get all prompts from the user before doing anything this way the whole 
+# script can be run without extra input from the user
+
+# Find untranslated file count
+count=$(find $UNTRANSLATED -maxdepth 1 -type f -name "*.atf" -printf x | wc -c)
+echo "Found $count .atf files in the untranslated directory"
+if [ ! $count -gt 0 ]
+then
+    read -p "Do you want to download the UrIII data set? (y/n default:n) " responce
+    [ ! -z $responce ] && ( [ ${responce:0:1} == 'y' ] || [ ${responce:0:1} == 'Y' ])
+    grabUntranslatedData=$?
+fi
+
+# Find translated file count
+count=$(find $TRANSLATED -maxdepth 1 -type f -name "*.conll" -printf x | wc -c)
+echo "Found $count .conll files in the translated directory"
+if [ ! $count -gt 0 ]
+then
+    read -p "Do you want to translate the untranslated data set? (y/n default:n) " responce
+    [ ! -z $responce ] && ( [ ${responce:0:1} == 'y' ] || [ ${responce:0:1} == 'Y' ])
+    translatedUntranslatedData=$?
+fi
+
+# Ask if pipeline should be removed after translation
+if [ $translatedUntranslatedData = 0 ]
+then
+    read -p "Do you want to remove the pipeline after translation? (y/n default:n) " responce
+    [ ! -z $responce ] && ( [ ${responce:0:1} == 'y' ] || [ ${responce:0:1} == 'Y' ])
+    removePipeline=$?
+fi
+
+# Ask if the database will be built
+read -p "Do you want to build the database? (y/n default:n) " responce
+[ ! -z $responce ] && ( [ ${responce:0:1} == 'y' ] || [ ${responce:0:1} == 'Y' ])
+buildDatabase=$?
+
+# Ask if the database will be populated (if it will be created)
+if [ $buildDatabase == 0 ]
+then
+    read -p "Do you want to populate the database? (y/n default:n) " responce
+    [ ! -z $responce ] && ( [ ${responce:0:1} == 'y' ] || [ ${responce:0:1} == 'Y' ])
+    populateDatabase=$?
+fi
+
+##################################
+########## Build Script ##########
+##################################
 
 ########## Translated Texts ##########
 # Link to untranslated Ur III period https://cdli.ucla.edu/tools/cdlifiles/cdli_ur3atf.zip
 # Link to the translation pipline https://github.com/cdli-gh/Sumerian-Translation-Pipeline.git
-# Download, parse, and translate the data as needed
 
-# Check if the untranslated dir has atf files
-count=$(find $UNTRANSLATED -maxdepth 1 -type f -name "*.atf" -printf x | wc -c)
-if [ $count -gt 0 ]
-then
-    echo "$UNTRANSLATED has $count atf entries"
-else
-    echo "$UNTRANSLATED has no atf entries"
-    read -p "Do you want to fetch the tablets from the Ur III period? (y/n default: n) " responce
-    if [ ! -z $responce ] && ( [ ${responce:0:1} == 'y' ] || [ ${responce:0:1} == 'Y' ] )
+# Download and parse the untranslated data as needed
+if [ $grabUntranslatedData = 0 ]
+    # Get the file from cdli if it does not exist
+    if [ -f "$UNTRANSLATED/UrIIItablets.zip" ]
     then
-        # Get the file from cdli if it does not exist
-        if [ -f "$UNTRANSLATED/UrIIItablets.zip" ]
-        then
-            echo "Zip file already downloaded"
-        else
-            curl -o $UNTRANSLATED/UrIIItablets.zip $CDLIDATA
-        fi
-
-        # Unzip the file
-        unzip $UNTRANSLATED/UrIIItablets.zip -d $UNTRANSLATED/
-
-        # Split the file into files and remove the default file
-        g++ -std=c++17 -o dataSplitter $SCRIPTPATH/C++/DataCollection/splitUntranslated.cpp
-        ./dataSplitter "$UNTRANSLATED/ur3_20110805_public.atf" "$UNTRANSLATED/"
-
-        # Clean up the directory
-        rm -r $UNTRANSLATED/__MACOSX # $UNTRANSLATED/UrIIItablets.zip
-        rm $UNTRANSLATED/ur3_20110805_public.atf dataSplitter
-    fi
-fi
-
-# Check if the translated directory has conll entries to use
-count=$(find $TRANSLATED -maxdepth 1 -type f -name "*.conll" -printf x | wc -c)
-if [ $count -gt 0 ]
-then
-    echo "$TRANSLATED has $count conll entries"
-else
-    echo "$TRANSLATED has no conll entries"
-    # Ask user if they want to create entries from untranslated data
-    read -p "Do you want to create tranlsated files? (y/n defuault: n) " responce
-    if [ ! -z $responce ] && ( [ ${responce:0:1} == 'y' ] || [ ${responce:0:1} == 'Y' ] )
-    then
-        #TODO: get translation pipeline and run the files through
-        echo "Unimplelmented atm"
+        echo "Zip file already downloaded"
     else
-        # We do not have anything to add to the database
-        read -p "Continue with making empty database? (y/n default:n) " responce
-        if [ ! -z $responce ] && ( [ ${responce:0:1} == 'y' ] || [ ${responce:0:1} == 'Y' ] )
-        then
-            echo "Contunuing to creating database"
-        else
-            exit 0
-        fi
+        curl -o $UNTRANSLATED/UrIIItablets.zip $CDLIDATA
     fi
+
+    # Unzip the file
+    unzip $UNTRANSLATED/UrIIItablets.zip -d $UNTRANSLATED/
+
+    # Split the file into files and remove the default file
+    g++ -std=c++17 -o dataSplitter $SCRIPTPATH/C++/DataCollection/splitUntranslated.cpp
+    ./dataSplitter "$UNTRANSLATED/ur3_20110805_public.atf" "$UNTRANSLATED/"
+
+    # Clean up the directory
+    rm -r $UNTRANSLATED/__MACOSX # $UNTRANSLATED/UrIIItablets.zip
+    rm $UNTRANSLATED/ur3_20110805_public.atf dataSplitter
 fi
 
-echo Contunuing
+# Translate the data as needed
+if [ $translatedUntranslatedData = 0 ]
+    # Grab the translation pipeline from github if it is not present
+    if [ ! -f "$SCRIPTPATH/Sumerian-Translation-Pipeline"]
+    then
+        git clone https://github.com/cdli-gh/Sumerian-Translation-Pipeline.git
+    fi
+    # TODO: Translate the Untranslated data
+fi
 
-# MySQL installed
-# 
+# Remove pipeline if requested
+if [ $removePipeline = 0 ]
+then
+    rm -r "$SCRIPTPATH/Sumerian-Translation-Pipeline"
+fi
+
+# TODO: Check that MySQL is installed
 
 # TODO: Setup database
 
