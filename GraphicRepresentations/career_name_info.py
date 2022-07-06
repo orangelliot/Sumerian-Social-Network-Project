bfaff14d35c2e3284b4673e10b78d6dcbc11fb66
Nicholas <nick.uhlhorn@gmail.com>
2022-07-05 22:32:16 -0700

    added basic structure of career graph gen

---
+++ GraphicRepresentations/career_name_info.py
@@ -0,0 +1,168 @@
+# Nicholas J Uhlhorn
+# Sumerian Social Network Research Project
+# July 2022
+
+import mysql.connector as connection
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+# Suppress warning that sql connector is not compatible with our database (seems to work fine so far)
+import warnings
+warnings.simplefilter(action='ignore', category=UserWarning)
+
+# Database password
+pwd_token_file = open("dbtoken.txt", 'r')
+pwd_token = pwd_token_file.readline()
+
+if (len(sys.argv) < 2):
+	print("Incorrect number of arguments, see usage:\npython career_name_info.py <name_of_interest> <name_of_interest> ...")
+	exit()
+
+# Get names to lookup
+names = sys.argv[1:]
+
+queryDFs = []
+
+# Open connection to database
+database = connection.connect(
+	host = "sumerian-social-network.clzdkdgg3zul.us-west-2.rds.amazonaws.com",
+	database = "sumerianDB",
+	user = "root",
+	password = pwd_token
+)
+
+# Get tablets
+# ASSUMPTION: since using distinct, assuming that multiple occurrences of a name on a tablet is the same person
+# SELECT DISTINCT rawnames.tabid, yearsequence.year, yearsequence.sequence
+# 	FROM rawnames INNER JOIN bestyears 
+# 		ON rawnames.tabid = bestyears.tabid 
+# 	INNER JOIN yearsequence
+# 		ON bestyears.year = yearsequence.year
+# 	WHERE rawnames.name = 'en-mah-gal-an-na';
+
+name_count = len(names)
+progress = 0
+
+print("Querying database:")
+for name in names:
+	# Set query to search for name
+	query = f"SELECT DISTINCT rawnames.tabid, yearsequence.year, yearsequence.sequence FROM rawnames INNER JOIN bestyears ON rawnames.tabid = bestyears.tabid INNER JOIN yearsequence ON bestyears.year = yearsequence.year WHERE rawnames.name = '{name}';"
+	# Query the database
+	queryDFs.append(pd.read_sql(query, database))
+	progress += 1
+	print(f"{progress}/{name_count}", end="\r")
+print(f"{progress}/{name_count}")
+
+# Close database as we are done with it
+database.close()
+
+from datetime import date
+import os
+import shutil
+
+try:
+	os.mkdir("output")
+except:
+	print()
+try:
+	os.mkdir("output/img")
+except:
+	print()
+
+# Start html file
+html_log = open("output/output.html", "w")
+
+todays_date = date.today()
+
+html_log.write(f"<html>\n<head>\n<title>{todays_date}</title>\n</head>\n<body>\n")
+
+# Generate bar graphs
+for i in range(name_count):
+	# Get instance of data
+	data_name = names[i]
+	data = queryDFs[i]
+
+	data = data.astype({'sequence': int})
+
+	# Extract data
+	count_data = data.groupby(['sequence'], as_index=False).agg({'sequence': 'first', 'year': ['first', 'count']})
+	years = count_data['sequence']['first']
+	counts = count_data['year']['count']
+	
+	plt.rcParams["figure.figsize"] = (20,12)
+	fig, bar_plt = plt.subplots(1,1)
+
+	bars = bar_plt.bar(years, counts)
+
+	bar_plt.set_title(f"Occurrence of \"{data_name}\"")
+	bar_plt.set_xlabel("Sequential Year")
+	bar_plt.set_ylabel("Tablet Count")
+	
+	bar_plt.set_xticks(years)
+	bar_plt.set_xticklabels(count_data['year']['first'], rotation=90)
+
+	bar_plt.bar_label(bars, padding=3)
+
+	# Save plot
+	image_file_name = f"occurrence_of_{data_name}.png"
+	plt.savefig(f"output\img\{image_file_name}", bbox_inches="tight")
+
+	# Save plot to html file
+	html_log.write(f"<h1>{data_name}</h1>\n")
+	html_log.write(f"<img src=\"img\{image_file_name}\">")
+
+##### Generate career tracker #####
+minYear = np.min(data['sequence'].astype('int'))
+maxYear = np.max(data['sequence'].astype('int'))
+maxCount = 0
+
+for data in queryDFs:
+	count_data = data.groupby(['sequence', 'year']).size().reset_index().rename(columns={0:''})
+	counts = count_data.iloc[:,2].to_numpy()
+	maxCount = max(maxCount, np.max(counts))
+
+fig, careerPlot = plt.subplots(1,1)
+for i in range(name_count):
+	# Get instance of data
+	data_name = names[i]
+	data = queryDFs[i]
+
+	data = data.astype({'sequence': int})
+
+	# Extract data
+	count_data = data.groupby(['sequence'], as_index=False).agg({'sequence': 'first', 'year': ['first', 'count']})
+	years = count_data['sequence']['first']
+	counts = count_data['year']['count']
+
+	# Get Scales for points
+	scales = [20 + (20*c) for c in counts]
+
+	minYear = min(minYear, np.min(years))
+	maxYear = max(maxYear, np.max(years))
+
+	careerPlot.plot(years, np.ones(len(years)) * i, label=names[i])
+	careerPlot.scatter(years, np.ones(len(years)) * i, label=names[i], marker='|', s=scales)
+
+
+	# for j in range(len(years)):
+	# 	careerPlot.annotate(counts[j], (years[j], i), xytext=(years[j], i+0.03))
+
+careerPlot.set_title('Career Tracking of Same Name on Nearby Tablets')
+careerPlot.set_ylabel("Name")
+careerPlot.set_xlabel("Relative Linear Date")
+
+careerPlot.set_yticks(np.arange(0,len(names)), names)
+careerPlot.set_xticks(np.arange(minYear, maxYear+1), np.arange(minYear, maxYear+1))
+
+# Save plot
+image_file_name = "careers.png"
+plt.savefig(f"output\img\{image_file_name}", bbox_inches="tight")
+
+# Save plot to html file
+html_log.write(f"<h1>Careers</h1>\n")
+html_log.write(f"<img src=\"img\{image_file_name}\">")
+
+html_log.write("</body>\n</html>")
+html_log.close()
