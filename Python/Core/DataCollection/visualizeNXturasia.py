3c648cc4fe3e9e9b959a986d2029bb7d75676208
elliot <elliotfisk@gmail.com>
2022-11-15 10:24:30 -0800

    forgot this thingy

---
+++ Python/Core/DataCollection/visualizeNXturasia.py
@@ -0,0 +1,92 @@
+import networkx as nx
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+from CSVToNX import CSVToNX
+
+G = CSVToNX(f'allPairs.csv').getNX()
+
+pos = nx.drawing.layout.spring_layout(G)
+nx.set_node_attributes(G, pos, 'pos')
+df = pd.read_csv('turaamili_siaa_babies.csv', names=['P1',])
+
+edge_x = []
+edge_y = []
+for edge in G.edges():
+    x0, y0 = G.nodes[edge[0]]['pos']
+    x1, y1 = G.nodes[edge[1]]['pos']
+    edge_x.append(x0)
+    edge_x.append(x1)
+    edge_x.append(None)
+    edge_y.append(y0)
+    edge_y.append(y1)
+    edge_y.append(None)
+
+edge_trace = go.Scatter(
+    x=edge_x, y=edge_y,
+    line=dict(width=0.5, color='#888'),
+    hoverinfo='none',
+    mode='lines')
+
+node_x = []
+node_y = []
+for node in G.nodes():
+    x, y = G.nodes[node]['pos']
+    node_x.append(x)
+    node_y.append(y)
+
+node_trace = go.Scatter(
+    x=node_x, y=node_y,
+    mode='markers',
+    hoverinfo='text',
+    marker=dict(
+        showscale=True,
+        # colorscale options
+        #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
+        #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
+        #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
+        colorscale='YlGnBu',
+        reversescale=True,
+        color=[],
+        size=10,
+        colorbar=dict(
+            thickness=15,
+            title='Node Connections',
+            xanchor='left',
+            titleside='right'
+        ),
+        line_width=2))
+
+node_in_babies = []
+node_text = []
+
+for name in (G.nodes.data()):
+    name = name[0]
+    if name == 'tu-ra-am-i3-li2' or name == 'SI-A-a':
+        node_in_babies.append(2)
+    elif (df['P1'].eq(name)).any():
+        node_in_babies.append(1)
+    else:
+        node_in_babies.append(0)
+    node_text.append(name)
+#print(len(node_in_babies))
+
+node_trace.marker.color = node_in_babies
+node_trace.text = node_text
+
+fig = go.Figure(data=[edge_trace, node_trace],
+             layout=go.Layout(
+                title='<br>Network graph made with Python',
+                titlefont_size=16,
+                showlegend=False,
+                hovermode='closest',
+                margin=dict(b=20,l=5,r=5,t=40),
+                annotations=[ dict(
+                    text="Python code: <a href='https://plotly.com/ipython-notebooks/network-graphs/'> https://plotly.com/ipython-notebooks/network-graphs/</a>",
+                    showarrow=False,
+                    xref="paper", yref="paper",
+                    x=0.005, y=-0.002 ) ],
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                )
+fig.show()
